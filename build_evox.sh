f79e89b908386b9d15bcf32dce33501f7905826a
Renelz <131983767+Renelzx@users.noreply.github.com>
2026-01-19 09:57:04 +0700

    Update build_evox.sh

--- build_evox.sh
+++ build_evox.sh
@@ -1,44 +1,55 @@
+echo ">>> Starting Cleanup..."
 # cleanup
 remove_lists=(
 .repo/local_manifests
 prebuilts/clang/host/linux-x86
-device/qcom/sepolicy
-device/qcom/sepolicy-legacy-um
-device/qcom/sepolicy_vndr/legacy-um
-device/asus/sdm660-common
-device/asus/X01BD
-kernel/asus/sdm660
-out/target/product/X01BD
-external/chromium-webview
+device/samsung_slsi/sepolicy
+device/samsung/universal9611-common
+device/samsung/a51
+kernel/samsung/universal9611
+out/target/product/a51
+vendor/samsung/a51
+hardware/samsung_slsi/libbt
 )
 
 rm -rf "${remove_lists[@]}"
 
 # init repo
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/rsuplaygrnd/evox_manifest.git -b bka -g default,-mips,-darwin,-notdefault
+echo ">>> Starting Initializing Repo..."
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs --depth 1
 
 # clone local manifests
-git clone https://github.com/rsuplaygrnd/local_manifest --depth 1 -b X01BD-16.0_EvoX .repo/local_manifests
+echo ">>> Cloning Local Manifests..."
+git clone https://github.com/Renelzx/local_manifest --depth 1 -b a51_16.0_EvoX .repo/local_manifests
 
 # repo sync
+echo ">>> Starting Repo Sync..."
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
 
-# setup KernelSU
-if [ -d kernel/asus/sdm660 ]; then 
-	cd kernel/asus/sdm660
-	curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
-	cd ../../..
+echo ">>> Fixing patch..."
+# Fix: hardware/samsung_slsi/libbt error "could not import blueprint"
+LIBBT_BP="hardware/samsung_slsi/libbt/Android.bp"
+if [ -f "$LIBBT_BP" ]; then
+    echo "  > Patching $LIBBT_BP..."
+    # Menambahkan bootstrap: true jika belum ada
+    if ! grep -q "bootstrap: true" "$LIBBT_BP"; then
+        sed -i 's/name: "libbt_vendor",/name: "libbt_vendor",\n    bootstrap: true,/g' "$LIBBT_BP"
+    fi
+else
+    echo "  ! Warning: $LIBBT_BP Tidak ditemukan. Mungkin sudah dihapus atau path berbeda."
 fi
 
 # Set up build environment
-export BUILD_USERNAME=rsuntk 
+echo ">>> Setup Environment..."
+export BUILD_USERNAME=renelzx 
 export BUILD_HOSTNAME=nobody 
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
 # Build the ROM
-lunch lineage_X01BD-bp2a-userdebug
+echo ">>> Starting Build..."
+lunch lineage_a51-bp4a-userdebug
 make installclean
 m evolution
 
-[ -d out ] && ls out/target/product/X01BD
+[ -d out ] && ls out/target/product/a51
